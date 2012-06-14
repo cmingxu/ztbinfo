cddb052c7df654f40ed6c611d37756f11843b31d
cming <cming.xu@photomugs.com>
2012-06-14 22:07:47 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ztbinfo::Application.config.secret_token = '45c21c54edd972f0ae856e6db26c6df7604d40ff7f7f9701d49be45c999fa6a9f0b59063dad028c4e52d8d36b2088b53a115c10e4cb13694723d31579b66a690'
